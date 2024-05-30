55ec0e45f33558885ff4c10cb905fcaa8b5ea85e
vimal D <144089192+Vimal007Vimal@users.noreply.github.com>
2024-05-30 15:35:50 +0530

    Create dbinfo.inc.php

---
+++ dbinfo.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'database-1.cvk4i8s0gb7i.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin123');
+define('DB_DATABASE', 'db');
+?>
